cb36f457dd1457db969b874d01c307c74fe77fdb
Irshad Mohammad <irshad@patterncomputer.com>
2019-01-30 14:56:12 -0800

    pneumonia first run

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/l26f810e9143s3y/pneumonia-yes-no.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/l26f810e9143s3y/pneumonia-yes-no.pkl?dl=1'
 export_file_name = 'pneumonia-yes-no.pkl'
 
 classes = ['NORMAL', 'PNEUMONIA']
