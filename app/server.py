77db343d8f06fc930f2bc28090e42ab708f348e5
Irshad Mohammad <irshad@patterncomputer.com>
2019-01-30 13:18:46 -0800

    minor

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from fastai.vision import *
 export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['black-me', 'grizzly-me', 'teddys-me']
 path = Path(__file__).parent
 
 app = Starlette()
